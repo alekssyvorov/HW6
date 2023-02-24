baf73b7f06255491bcab851f0dccbb20f09c1d1d
Evgeniy <alekssyvorov29@gmail.com>
2023-02-24 15:49:46 +0200

    Добавьте файлы проекта.

---
+++ HW6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6
+{
+    internal class Program
+    {
+        static int SumTo(int a)
+        {
+            if (a == 1)
+                return 1;
+            else
+                return a + SumTo(a-1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumTo(9));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HW6_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "5";
+            string b = "1";
+
+            Console.WriteLine(Add(a, b));
+
+            Console.ReadLine();
+        }
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        static string Add(string a, string b)
+        {
+            return a + b;
+        }
+    }
+}
